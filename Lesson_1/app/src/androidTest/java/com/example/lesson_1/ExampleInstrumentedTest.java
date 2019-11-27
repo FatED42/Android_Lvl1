f87aecb6d41fa277775eb07b06c22c8c5b993cd7
FatED42 <cjkrol42@gmail.com>
2019-11-27 21:38:06 +0300

    Дз к 1 лекции

--- Lesson_1/app/src/androidTest/java/com/example/lesson_1/ExampleInstrumentedTest.java
+++ Lesson_1/app/src/androidTest/java/com/example/lesson_1/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.lesson_1;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
